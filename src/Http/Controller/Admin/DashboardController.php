3b62aaaa3da16fc7c07cea067fb5a1e9eae69a28
Hugo Alliaume <hugo@alliau.me>
2024-03-30 10:26:48 +0100

    feat: protect admin behind access control

---
+++ src/Http/Controller/Admin/DashboardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controller\Admin;
+
+use App\Domain\Blog as Blog;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/home.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Hugo Alliaume')
+            ->setFaviconPath('images/icons/favicon.svg')
+            ->generateRelativeUrls()
+        ;
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        //yield MenuItem::linkToCrud('The Label', 'fas fa-list', Blog\Post::class);
+    }
+}
