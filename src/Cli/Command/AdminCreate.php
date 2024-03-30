71abb6deadc096b2aa4fde321bd38a4e07401543
Hugo Alliaume <hugo@alliau.me>
2024-03-30 10:24:55 +0100

    feat: setup auth and admin creation

---
+++ src/Cli/Command/AdminCreate.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Cli\Command;
+
+use App\Domain\Auth\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:admin:create',
+    description: 'Create a new admin user',
+)]
+final class AdminCreate extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher,
+    )
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $username = $io->ask('Username');
+        $password = $io->askHidden('Password');
+
+        $user = new User();
+        $user->setUsername($username);
+        $user->setPassword($this->passwordHasher->hashPassword($user, $password));
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $io->success('Admin user created successfully!');
+
+        return Command::SUCCESS;
+    }
+}
