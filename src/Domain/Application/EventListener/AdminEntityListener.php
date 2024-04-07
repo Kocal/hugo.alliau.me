3b031fd2ca96f6a29894bfea08edc20d7d4fe52d
Hugo Alliaume <hugo@alliau.me>
2024-04-07 10:04:24 +0200

    chore: log AdminEntityListener

--- src/Domain/Application/EventListener/AdminEntityListener.php
+++ src/Domain/Application/EventListener/AdminEntityListener.php
@@ -8,19 +8,26 @@ use App\Http\Cache\ValueObject\CacheItem;
 use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityDeletedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityPersistedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityUpdatedEvent;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 
 final class AdminEntityListener
 {
     public function __construct(
         private HttpCache $httpCache,
+        private LoggerInterface $logger,
     ) {
     }
 
     #[AsEventListener]
-    public function afterEntityPersist(AfterEntityPersistedEvent $event): void
+    public function afterEntityPersisted(AfterEntityPersistedEvent $event): void
     {
         $entity = $event->getEntityInstance();
+
+        $this->logger->info('Got event {event_class}.', [
+            'event_class' => $event::class,
+            'entity_class' => $entity::class,
+        ]);
 
         if ($entity instanceof CacheableEntity) {
             $this->httpCache->clearFor(CacheItem::fromEntity($entity));
@@ -28,9 +35,14 @@ final class AdminEntityListener
     }
 
     #[AsEventListener]
-    public function afterEntityUpdate(AfterEntityUpdatedEvent $event): void
+    public function afterEntityUpdated(AfterEntityUpdatedEvent $event): void
     {
         $entity = $event->getEntityInstance();
+
+        $this->logger->info('Got event {event_class}.', [
+            'event_class' => $event::class,
+            'entity_class' => $entity::class,
+        ]);
 
         if ($entity instanceof CacheableEntity) {
             $this->httpCache->clearFor(CacheItem::fromEntity($entity));
@@ -38,9 +50,14 @@ final class AdminEntityListener
     }
 
     #[AsEventListener]
-    public function afterEntityRemove(AfterEntityDeletedEvent $event): void
+    public function afterEntityDeleted(AfterEntityDeletedEvent $event): void
     {
         $entity = $event->getEntityInstance();
+
+        $this->logger->info('Got event {event_class}.', [
+            'event_class' => $event::class,
+            'entity_class' => $entity::class,
+        ]);
 
         if ($entity instanceof CacheableEntity) {
             $this->httpCache->clearFor(CacheItem::fromEntity($entity));
