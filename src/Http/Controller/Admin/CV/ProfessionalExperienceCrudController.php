6582881c0abf1e740ef44b712bf1018fd746bde1
Hugo Alliaume <hugo@alliau.me>
2024-04-07 20:30:26 +0200

    feat(admin): configure ProfessionalExperience fields

--- src/Http/Controller/Admin/CV/ProfessionalExperienceCrudController.php
+++ src/Http/Controller/Admin/CV/ProfessionalExperienceCrudController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controller\Admin\CV;
 
 use App\Domain\CV\ProfessionalExperience;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
 
 class ProfessionalExperienceCrudController extends AbstractCrudController
 {
@@ -12,14 +18,19 @@ class ProfessionalExperienceCrudController extends AbstractCrudController
         return ProfessionalExperience::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            IdField::new('id')->setDisabled(),
+            TextField::new('company'),
+            UrlField::new('url'),
+            TextField::new('jobName'),
             TextEditorField::new('description'),
+            DateField::new('startDate'),
+            DateField::new('endDate'),
+            ArrayField::new('badges'),
         ];
     }
-    */
+
 }
