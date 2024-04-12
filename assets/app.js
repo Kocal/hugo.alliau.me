242d7f36d902ec7e741d02eedb7a27b7e66ae5b4
Hugo Alliaume <hugo@alliau.me>
2024-04-12 22:51:21 +0200

    chore: remove Shiki

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,3 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
