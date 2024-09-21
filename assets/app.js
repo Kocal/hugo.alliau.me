153e4e9be63796f5427a65384c29b79f3ee5745d
Hugo Alliaume <hugo@alliau.me>
2024-09-21 22:11:18 +0200

    imp(scripts): delete bootstrap.js file, reduce the number of requests

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,4 @@
-import "./bootstrap.js";
-
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
+import { startStimulusApp } from "@symfony/stimulus-bundle";
 import "./styles/app.css";
+
+const app = startStimulusApp();
