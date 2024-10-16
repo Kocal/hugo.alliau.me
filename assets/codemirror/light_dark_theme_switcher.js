67e448bc795495b991ecdb102a9f12f0f5eaa325
Hugo Alliaume <hugo@alliau.me>
2024-10-16 08:22:32 +0200

    feat(codemirror): implement other actions and refactor

--- assets/codemirror/light_dark_theme_switcher.js
+++ assets/codemirror/light_dark_theme_switcher.js
@@ -1,30 +1,36 @@
-import {Compartment} from "@codemirror/state";
+import { Compartment } from "@codemirror/state";
 import { EditorView } from "@codemirror/view";
 
-export function lightDarkThemeSwitcher({lightTheme, darkTheme}) {
+export function lightDarkThemeSwitcher({ lightTheme, darkTheme }) {
     if (!lightTheme) {
         throw new Error(`Parameter 'lightTheme' is required.`);
     }
-    
+
     if (!darkTheme) {
         throw new Error(`Parameter 'darkTheme' is required.`);
     }
 
     const themeCompartment = new Compartment();
-    
+
     return [
-        themeCompartment.of(window.matchMedia("(prefers-color-scheme: dark)").matches ? darkTheme : lightTheme),
+        themeCompartment.of(
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? darkTheme
+                : lightTheme,
+        ),
 
         EditorView.domEventHandlers({
             window: {
                 matchMedia: (event) => {
                     if (event.media === "(prefers-color-scheme: dark)") {
                         event.target.dispatch({
-                            effects: themeCompartment.reconfigure(event.matches ? darkTheme : lightTheme)
+                            effects: themeCompartment.reconfigure(
+                                event.matches ? darkTheme : lightTheme,
+                            ),
                         });
                     }
-                }
-            }
-        })
+                },
+            },
+        }),
     ];
 }
--- assets/controllers/markdown_editor_controller.js
+++ assets/controllers/markdown_editor_controller.js
@@ -1,91 +1,154 @@
-import {Controller} from "@hotwired/stimulus";
-import {basicSetup, EditorView} from "codemirror"
-import {markdown} from "@codemirror/lang-markdown"
-import {dracula, tomorrow} from "thememirror";
-import {Compartment, EditorSelection} from "@codemirror/state";
-import {undo as commandUndo, redo as commandRedo} from '@codemirror/commands';
-import {codeLanguages} from "../codemirror/code_languages.js";
-import {lightDarkThemeSwitcher} from "../codemirror/light_dark_theme_switcher.js";
-
+import { redo as commandRedo, undo as commandUndo } from "@codemirror/commands";
+import { markdown } from "@codemirror/lang-markdown";
+import { Compartment, EditorSelection } from "@codemirror/state";
+import { Controller } from "@hotwired/stimulus";
+import { EditorView, basicSetup } from "codemirror";
+import { dracula, tomorrow } from "thememirror";
+import { codeLanguages } from "../codemirror/code_languages.js";
+import { lightDarkThemeSwitcher } from "../codemirror/light_dark_theme_switcher.js";
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
-    static targets = [
-        'textarea',
-    ]
+    static targets = ["textarea"];
 
     connect() {
         const editorTheme = new Compartment();
-        
+
         this.view = new EditorView({
             doc: this.textareaTarget.value,
             extensions: [
                 basicSetup,
-                markdown({codeLanguages}),
-                lightDarkThemeSwitcher({darkTheme: dracula, lightTheme: tomorrow}),
-                EditorView.updateListener.of(viewUpdate => {
-                    this.textareaTarget.value = viewUpdate.view.state.doc.toString();
+                markdown({ codeLanguages }),
+                lightDarkThemeSwitcher({
+                    darkTheme: dracula,
+                    lightTheme: tomorrow,
+                }),
+                EditorView.updateListener.of((viewUpdate) => {
+                    this.textareaTarget.value =
+                        viewUpdate.view.state.doc.toString();
                 }),
             ],
-        })
+        });
         // this.view.dom.style.width = this.textareaTarget.clientWidth + 'px';
-        this.view.dom.style.maxHeight = '85dvh'; // hard-coded, can be improved later
-        this.textareaTarget.parentNode.insertBefore(this.view.dom, this.textareaTarget)
+        this.view.dom.style.maxHeight = "85dvh"; // hard-coded, can be improved later
+        this.textareaTarget.parentNode.insertBefore(
+            this.view.dom,
+            this.textareaTarget,
+        );
         this.textareaTarget.style.display = "none";
     }
-    
+
     undo() {
-        commandUndo(this.view)
+        commandUndo(this.view);
         this.view.focus();
     }
-    
+
     redo() {
-        commandRedo(this.view)
+        commandRedo(this.view);
         this.view.focus();
     }
 
     bold() {
-        this.view.dispatch(this.view.state.changeByRange(range => ({
-            changes: [{from: range.from, insert: "**"}, {from: range.to, insert: "**"}],
-            range: EditorSelection.range(range.from, range.to + 4)
-        })));
-        this.view.dispatch({
-            selection: {
-                anchor: this.view.state.selection.ranges[0].from + 2,
-                head: this.view.state.selection.ranges[0].from + 2,
-            }
-        })
-        this.view.focus();
+        this.#insertSyntax("**${cursor}**");
     }
-    italic() {
-        this.view.dispatch(this.view.state.changeByRange(range => ({
-            changes: [{from: range.from, insert: "_"}, {from: range.to, insert: "_"}],
-            range: EditorSelection.range(range.from, range.to + 2)
-        })));
-        this.view.dispatch({ 
-            selection: {
-                anchor: this.view.state.selection.ranges[0].from + 1,
-                head: this.view.state.selection.ranges[0].from + 1,
-            }
-        })
-        this.view.focus();
-    }
-    strikethrough() {
-        this.view.dispatch(this.view.state.changeByRange(range => ({
-            changes: [{from: range.from, insert: "~"}, {from: range.to, insert: "~"}],
-            range: EditorSelection.range(range.from, range.to + 2)
-        })));
 
+    italic() {
+        this.#insertSyntax("_${cursor}_");
+    }
+
+    strikethrough() {
+        this.#insertSyntax("~~${cursor}~~");
+    }
+
+    /**
+     * TODO: can be improved to ensure the header is added at the start of the line
+     */
+    header(event) {
+        const { level } = event.params;
+        if (typeof level !== "number") {
+            throw new Error(
+                'Argument "level" is required and must be a number.',
+            );
+        }
+
+        this.#insertSyntax(`${"#".repeat(level)} \${cursor}`);
+    }
+
+    /**
+     * TODO: can be improved to add a link to the selected text
+     */
+    link() {
+        this.#insertSyntax("[${cursor}](https://)");
+    }
+
+    image() {
+        this.#insertSyntax("![](${cursor})(https://)");
+    }
+
+    codeBlock() {
+        this.#insertSyntax("```lang [file.ext]\n${cursor}\n```");
+    }
+
+    quote() {
+        this.#insertSyntax("> ${cursor}");
+    }
+
+    list(event) {
+        const type =
+            typeof event.params.type === "string"
+                ? event.params.type
+                : "unordered";
+        if (type !== "unordered" && type !== "ordered") {
+            throw new Error(
+                'Argument "type" must be either "ordered" or "unordered".',
+            );
+        }
+        this.#insertSyntax(`${type === "unordered" ? "-" : "1."} \${cursor}`);
+    }
+
+    alert(event) {
+        const type =
+            typeof event.params.type === "string" ? event.params.type : "info";
+        const validTypes = ["info", "tip", "warning", "important", "caution"];
+        if (!validTypes.includes(type)) {
+            throw new Error(
+                `Argument "type" must be one of ${validTypes.join(", ")}.`,
+            );
+        }
+
+        this.#insertSyntax(`> [!${type.toUpperCase()}]\n> \${cursor}`);
+    }
+    
+    #insertSyntax(text) {
+        const cursorId = "${cursor}";
+        const cursorIndex = text.indexOf(cursorId);
+        if (cursorIndex === -1) {
+            throw new Error(`Text must include the cursor placeholder: ${cursorId}`);
+        }
+    
+        text = text.replace(cursorId, "");
+        
+        this.view.dispatch(
+            this.view.state.changeByRange((range) => ({
+                changes: [{ from: range.from, insert: text }],
+                range: EditorSelection.range(
+                    range.from,
+                    range.to + text.length,
+                ),
+            })),
+        );
+        
         this.view.dispatch({
             selection: {
-                anchor: this.view.state.selection.ranges[0].from + 1,
-                head: this.view.state.selection.ranges[0].from + 1,
-            }
-        })
+                anchor: this.view.state.selection.ranges[0].from + cursorIndex,
+                head: this.view.state.selection.ranges[0].from + cursorIndex,
+            },
+        });
+        
         this.view.focus();
     }
 
     disconnect() {
-        this.view.destroy()
+        this.view.destroy();
     }
 }
