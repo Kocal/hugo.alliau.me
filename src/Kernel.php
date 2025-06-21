fb5bf3f515cb8120c3fb69c50a01e35e202a139f
Hugo Alliaume <hugo@alliau.me>
2025-06-21 09:03:02 +0200

    Upgrade symfony/framework-bundle recipe

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,11 +5,8 @@ declare(strict_types=1);
 namespace App;
 
 use App\Shared\Domain\CQRS\AsCommandHandler;
-use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
-use Doctrine\ORM\Mapping\ClassMetadata;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\ChildDefinition;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
@@ -24,18 +21,6 @@ class Kernel extends BaseKernel
             $definition->addTag('messenger.message_handler', [
                 'bus' => 'messenger.bus.command',
             ]);
-        });
-
-        $container->addCompilerPass(new class() implements CompilerPassInterface {
-            public function process(ContainerBuilder $container): void
-            {
-                $container->getDefinition('doctrine.orm.default_configuration')
-                    ->addMethodCall('setIdentityGenerationPreferences', [
-                        [
-                            PostgreSQLPlatform::class => ClassMetadata::GENERATOR_TYPE_SEQUENCE,
-                        ],
-                    ]);
-            }
         });
     }
 }
--- src/Shared/Infrastructure/EasyAdmin/Controller/DashboardController.php
+++ src/Shared/Infrastructure/EasyAdmin/Controller/DashboardController.php
@@ -13,7 +13,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
