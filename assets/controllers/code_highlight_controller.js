ebab7f4902996d449429c3bb96623b9f053f1e4e
Hugo Alliaume <hugo@alliau.me>
2025-10-26 09:18:41 +0100

    Shiki transformers

--- assets/controllers/code_highlight_controller.js
+++ assets/controllers/code_highlight_controller.js
@@ -1,14 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 import { codeToHtml } from 'https://esm.sh/shiki@3.13.0'
+import { transformerNotationDiff, transformerNotationHighlight } from 'https://esm.sh/@shikijs/transformers@3.13.0'
 import { useIntersection } from 'stimulus-use'
-
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
-    const
     connect() {
         useIntersection(this)
-
     }
 
     async appear() {
@@ -21,6 +19,10 @@ export default class extends Controller {
         this.element.outerHTML = await codeToHtml(this.element.textContent, {
             lang: this.element.dataset.lang || 'plaintext',
             theme: 'one-dark-pro',
+            transformers: [
+                transformerNotationDiff(),
+                transformerNotationHighlight(),
+            ]
         });
     }
 }
