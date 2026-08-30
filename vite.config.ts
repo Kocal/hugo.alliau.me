ab716309da02527190ab37082c329d64874130d8
Hugo Alliaume <hugo@alliau.me>
2026-08-30 07:54:34 +0200

    Update JS/PHP dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,9 @@ import Symfony from "@symfony/reprise/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                app: "./assets/app.js",
-                admin: "./assets/admin.js",
-            },
-        },
+    input: {
+        app: "./assets/app.js",
+        admin: "./assets/admin.js",
     },
     plugins: [
         tailwindcss(),
